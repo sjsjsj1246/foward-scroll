f981fd24ca688669e35128cfd6cd472db3a45618
sjsjsj1246 <sjsjsj1246@gmail.com>
2024-04-25 01:53:18 +0900

    feat: progress bar

--- js/wall3d.js
+++ js/wall3d.js
@@ -3,6 +3,7 @@ const HOUSE_BASE_Z = -490
 class Wall3D {
     _scrollRange
     $house
+    $progressBar
 
     constructor() {
         this.$house = document.querySelector('.house')
@@ -11,13 +12,20 @@ class Wall3D {
         window.addEventListener('resize', this._resizeHandler.bind(this))
     }
 
+    get _scrollRatio() {
+        return window.scrollY / this._scrollRange
+    }
+
     _resizeHandler() {
         this._scrollRange = document.body.offsetHeight - window.innerHeight
     }
 
     _scrollHandler() {
-        const zMove = (window.scrollY / this._scrollRange) * 980 + HOUSE_BASE_Z
+        const zMove = this._scrollRatio * 980 + HOUSE_BASE_Z
         this.$house.style.transform = `translateZ(${zMove}vw)`
+
+        this.$progressBar = document.querySelector('.progress-bar')
+        this.$progressBar.style.width = `${this._scrollRatio * 100}%`
     }
 }
 
