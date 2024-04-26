cc845e50df273ae63b31ada909c210e3e2351d09
sjsjsj1246 <sjsjsj1246@gmail.com>
2024-04-27 01:02:01 +0900

    feat: 캐릭터 구현

--- js/wall3d.ts
+++ js/wall3d.ts
@@ -1,3 +1,5 @@
+import { Character } from './Character.js';
+
 const HOUSE_BASE_Z = -490;
 
 class Wall3D {
@@ -15,6 +17,7 @@ class Wall3D {
         window.addEventListener('scroll', this._scrollHandler.bind(this));
         window.addEventListener('resize', this._resizeHandler.bind(this));
         window.addEventListener('mousemove', this._mouseMoveHandler.bind(this));
+        window.addEventListener('click', this._handleClick.bind(this));
     }
 
     private get _scrollRatio(): number {
@@ -36,6 +39,10 @@ class Wall3D {
         this._mousePosition.x = -1 + (e.clientX / window.innerWidth) * 2;
         this._mousePosition.y = 1 - (e.clientY / window.innerHeight) * 2;
         this.$stage.style.transform = `rotateX(${this._mousePosition.y * 5}deg) rotateY(${this._mousePosition.x * 5}deg)`;
+    }
+
+    private _handleClick(e: MouseEvent): void {
+        new Character({ xPos: (e.clientX / window.innerWidth) * 100, speed: Math.random() * 0.5 + 0.2 });
     }
 }
 
